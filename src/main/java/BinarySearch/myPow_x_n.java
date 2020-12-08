edca464d8462f4ed271b854b1ae911ec0e1c3fae
Aways-Away <aways_away@163.com>
2020-12-08 18:47:41 +0800

    update

--- src/main/java/BinarySearch/myPow_x_n.java
+++ src/main/java/BinarySearch/myPow_x_n.java
@@ -8,7 +8,7 @@ public class myPow_x_n {
     public double myPow(double x, int n) {
         double res = 1.0;
         for (int i = n; i != 0; i = i / 2) {
-            if (i != 2) {
+            if (i % 2 != 0) {
                 res = res * x;
             }
             x = x * x;
--- src/test/java/BinaeySearch/myPowTest.java
+++ src/test/java/BinaeySearch/myPowTest.java
@@ -2,15 +2,21 @@ package BinaeySearch;
 
 import BinarySearch.myPow_x_n;
 
+import java.util.Scanner;
+
 /**
  * Created by LiuCui on 2020/12/7
  */
 public class myPowTest {
-    public static void main(String[] args){
-        double x=2.10000;
-        int n=3;
-        double res = new myPow_x_n().myPow(x,n);
-        System.out.println("输出："+res);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input (double) x: ");
+        double x = in.nextDouble();
+        System.out.println("Input (int) n: ");
+        int n = in.nextInt();
+
+        double res = new myPow_x_n().myPow(x, n);
+        System.out.println("输出：" + res);
     }
 }
 
