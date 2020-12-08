89e740241be1019b175d4d1ea1872747bea7a827
Aways-Away <aways_away@163.com>
2020-12-08 16:38:24 +0800

    data structure

---
+++ src/main/java/BinarySearch/Pow_x_n.java
@@ -0,0 +1,7 @@
+package BinarySearch;
+
+/**
+ * Created by LiuCui on 2020/12/7
+ */
+public class Pow_x_n {
+}
---
+++ src/main/java/BinarySearch/SearchSortedArray.java
@@ -0,0 +1,37 @@
+package BinarySearch;
+
+/**
+ * Created by LiuCui on 2020/12/7
+ */
+public class SearchSortedArray {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == nums[mid]) return mid;
+            if (nums[mid] >= nums[left]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            if (nums[mid] <= nums[right]) {
+                if (target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
